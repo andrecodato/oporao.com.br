390e7f67d47da2ae893f3ce2bb9da7dabd5841a8
André Codato <andrecodato@gmail.com>
2022-08-17 18:06:15 -0300

    commit

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src={images.chef} alt='chef' />
+    </div>
+    
+    <div className='app__wrapper_info'>
+      <SubHeading title='Palavras do Chef' />
+      <h1 className='headtext__cormorant'>No que acreditamos</h1>
+
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt='quote' />
+          <p className='p__opensans'>Meia noite eu te conto. Meia noite eu te conto. Meia noite eu te conto.</p>
+        </div>
+        <p className='p__opensans'>Meia noite eu te conto. Meia noite eu te conto. Meia noite eu te conto. Meia noite eu te conto. </p>
+      </div>
+
+      <div className='app__chef-sign'>
+        <p>Pedro Gabriel</p>
+        <p className='p__opensans'>Sócio e Chef</p>
+        <img src={images.sign} alt='sign' />
+      </div>
+    </div>
   </div>
 );
 
--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
 import './Laurels.css';
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className='app__bg app__wrapper section__padding' id='awards'>
+    <div className='app__wrapper_info'>
+      <SubHeading title='Premiações' />
+      <h1 className='headtext__cormorant'>Nossos troféis</h1>
+
+      <div className='app__laurels_awards'>
+        {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
+      </div>
+    </div>
+    
+    <div className='app__wrapper_img'>
+
+    </div>
   </div>
 );
 
